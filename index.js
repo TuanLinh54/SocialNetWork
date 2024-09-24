ad7a1a2c2435d8e7ce0a19a97e85bffe2ee8ac5b
TuanLinh54 <tlinh020405@gmail.com>
2024-09-24 13:53:04 +0700

    fix cors

--- index.js
+++ index.js
@@ -37,9 +37,33 @@ app.use(cors());
 //     origin: 'http://localhost:3000'
 // }));
 app.use(cors({
-    origin: ['https://social-net-work-fe.vercel.app/'],
+    origin: ['https://social-net-work-fe.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 }));
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "https://social-net-work-fe.vercel.app");
+
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
